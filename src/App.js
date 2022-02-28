7fa427c555a239972c135dcc2427e90720d87891
shinajite <73887414+shinajite@users.noreply.github.com>
2022-02-28 22:21:46 +0900

    display

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import "./App.css";
 import drone1 from "./drone1.jpg";
+import drone2 from "./drone2.jpg";
+import drone3 from "./drone3.jpg";
+import drone4 from "./drone4.jfif";
 import favorite from "./favorite.svg";
 import droneIcon from "./droneIcon.svg";
-
 
 function App() {
   return (
     <div className="App">
-
       <div className="header">
         <header>Drones</header>
-        
-        <input type="text"></input>
+
+        <input type="text" className="searchBar"></input>
       </div>
 
       <div className="switch">
@@ -21,17 +22,35 @@ function App() {
 
       <div className="body">
         <div className="section">
-        <img src={drone1} className="image"/>
-        <img src={favorite} className="icon"/>
-        <div className="droneName">レースドローン</div>
-        <div className="date">2022/02/28</div>
+          <img src={drone1} className="image" />
+          <img src={favorite} className="icon" />
+          <div className="droneName">レースドローン</div>
+          <div className="date">2022/02/28</div>
+        </div>
+        <div className="section">
+          <img src={drone2} className="image" />
+          <img src={favorite} className="icon" />
+          <div className="droneName">ドローンA</div>
+          <div className="date">2022/02/28</div>
+        </div>
+        <div className="section">
+          <img src={drone3} className="image" />
+          <img src={favorite} className="icon" />
+          <div className="droneName">ドローンA</div>
+          <div className="date">2022/02/28</div>
+        </div>
+        <div className="section">
+          <img src={drone4} className="image" />
+          <img src={favorite} className="icon" />
+          <div className="droneName">ドローンA</div>
+          <div className="date">2022/02/28</div>
         </div>
       </div>
 
       <div className="navbar">
-        <div><img src={droneIcon}/> </div>
-        <div><img src={droneIcon}/> </div>
-        <div><img src={droneIcon}/> </div>
+        <img src={droneIcon} className="navicon" />
+        <img src={droneIcon} className="navicon" />
+        <img src={droneIcon} className="navicon" />
       </div>
     </div>
   );
