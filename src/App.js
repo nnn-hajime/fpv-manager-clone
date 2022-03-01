711094c455bdf02da1c0997a68ccd041c0da8727
shinajite <73887414+shinajite@users.noreply.github.com>
2022-03-01 12:47:46 +0900

    menu bar

--- src/App.js
+++ src/App.js
@@ -5,22 +5,26 @@ import drone3 from "./drone3.jpg";
 import drone4 from "./drone4.jfif";
 import favorite from "./favorite.svg";
 import droneIcon from "./droneIcon.svg";
+import addIcon from "./add.svg";
+import menu from "./menu.svg";
 
 function App() {
   return (
     <div className="App">
       <div className="header">
-        <header>Drones</header>
-
-        <input type="text" className="searchBar"></input>
-      </div>
-
-      <div className="switch">
-        <div>ALL</div>
-        <div>FAVORITE</div>
+        <img src={menu} className="menu"/>
+        <div className="headerTop">
+          <h3>Drones</h3>
+          <input type="text" className="searchBar" placeholder="検索"></input>
+        </div>
       </div>
 
       <div className="body">
+        <div className="switch">
+          <div>ALL</div>
+          <div>FAVORITE</div>
+        </div>
+      
         <div className="section">
           <img src={drone1} className="image" />
           <img src={favorite} className="icon" />
@@ -46,7 +50,13 @@ function App() {
           <div className="date">2022/02/28</div>
         </div>
       </div>
-
+      <div className="addButton">
+        <div className="circle">
+         
+            <img src={addIcon} className="addIcon"/>
+        
+        </div>
+      </div>
       <div className="navbar">
         <img src={droneIcon} className="navicon" />
         <img src={droneIcon} className="navicon" />
